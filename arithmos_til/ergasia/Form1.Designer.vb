7347bf108976077b56f4e0d4280001700221e6d0
MariaChristou <mariachristou2@hotmail.com>
2017-01-15 19:51:41 +0200

    Add files via upload

---
+++ arithmos_til/ergasia/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(161, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.Button1.Location = New System.Drawing.Point(95, 144)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(81, 24)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Okey!"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(161, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(45, 96)
+        Me.TextBox1.MaxLength = 10
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(202, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Font = New System.Drawing.Font("Monotype Corsiva", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.Location = New System.Drawing.Point(45, 46)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(202, 18)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "Give Me Your Phone Number!"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ arithmos_til/ergasia/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim selStart As Integer = TextBox1.SelectionStart
+        Dim selMoveLeft As Integer = 0
+        Dim newStr As String = ""
+        Dim array(2) As Integer
+
+
+        For i As Integer = 0 To TextBox1.Text.Length - 1
+
+            If "0123456789".IndexOf(TextBox1.Text(i)) <> -1 Then
+                newStr = newStr & TextBox1.Text(i)
+
+            ElseIf i < selStart Then
+                selMoveLeft = selMoveLeft + 1
+
+            End If
+        Next
+        TextBox1.Text = newStr
+        TextBox1.SelectionStart = selStart - selMoveLeft
+
+        Dim Price As Integer
+        Dim firstTwoDigits As String = Int32.TryParse(newStr, Price)
+        firstTwoDigits = TextBox1.Text.ToString()
+        Dim Substring As String = firstTwoDigits.Substring(0, 2)
+
+        If TextBox1.Text.Length < 10 Then
+            MessageBox.Show("Less Than 10 Digits...Check The Number Again!")
+        ElseIf TextBox1.Text.Length = 10 Then
+
+            If Substring = "69" Then
+                MessageBox.Show("Thank You!")
+            ElseIf Substring <> "69" Then
+                MessageBox.Show("Only Greek Numbers Please!")
+            End If
+        End If
+
+
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+
+
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles TextBox1.TextChanged
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+End Class
